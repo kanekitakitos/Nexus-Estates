1905ac96c1c3726926aee81f7d1a2118559f9bf8
truta02 <167800591+truta02@users.noreply.github.com>
2026-02-23 18:12:53 +0000

    feat(register): botões tenta se comunicar com o backend
    -adição de logica dos botões para gerir o respostas e evitar reenvios desnecessários
    
    -botões usão as as pré-configurações do axios (@/lib/axiosAPI) para tentarem comunicar com o backend

--- frontend/src/components/layout/auth/register-form.tsx
+++ frontend/src/components/layout/auth/register-form.tsx
@@ -15,8 +15,70 @@ import {
   FieldLabel,
 } from "@/components/ui/forms/field"
 import { Input } from "@/components/ui/forms/input"
+import { useState } from "react";
+import { usersAxios } from "@/lib/axiosAPI";
 
 export function RegisterForm() {
+
+  const [isTryingRegister, setIsTryingRegister] = useState(false);
+
+  
+  // TODO: Refactor implementation to simplify and improve error handling  
+  async function handleRegister() {
+    if (isTryingRegister) return; // Prevent multiple register attempts at the same time
+
+    setIsTryingRegister(true);
+
+    var email = (document.getElementById("email") as HTMLInputElement).value
+    var password = (document.getElementById("password") as HTMLInputElement).value
+    var passwordConfirm = (document.getElementById("confirm-password") as HTMLInputElement).value
+
+    if (password !== passwordConfirm) {
+      alert("As senhas não coincidem. Por favor, tente novamente.");
+      setIsTryingRegister(false);
+
+      // Clear password fields
+      (document.getElementById("password") as HTMLInputElement).value = "";
+      (document.getElementById("confirm-password") as HTMLInputElement).value = "";
+      return;
+    }
+
+    await usersAxios.put("/auth/register", JSON.stringify({email, password}), {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      if (response.status === 200) { // Assuming a successful login returns a 200 status code
+        console.log("Registro efetuado!", response.data);
+        alert("Conta criada com sucesso!");
+      }
+      else {
+        console.log("resposta:", response);
+      }
+    })
+    .catch(error => {
+      console.error("error:", error);
+      if (error.response) {
+        console.error("Mensagem do servidor:", error.response.data);
+          switch (error.response.status) {
+            case 401: alert("Email ou senha incorretos. Tente novamente."); break;
+            case 404: alert("Usuário não encontrado. Verifique seu email ou registre-se."); break;
+            default: alert("Erro no servidor. Status: " + error.response.status); break;
+          }
+      }
+      else if (error.request) {
+        console.error("Requisição feita, mas sem resposta:", error.request);
+        alert("Nenhuma resposta do servidor. Verifique sua conexão.");
+      }
+      else {
+        console.error("Erro ao configurar a requisição :", error.message);
+        alert("Erro ao configurar a requisição da mensagem: " + error.message);
+      }
+    });
+
+    setIsTryingRegister(false);
+  }
 
   return (
     <div className={"flex flex-col gap-6"}>
