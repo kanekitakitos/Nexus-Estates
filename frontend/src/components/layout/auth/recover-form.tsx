28d81f5592a859955057fb6e116e2daf3ce5ad04
truta02 <167800591+truta02@users.noreply.github.com>
2026-02-25 21:37:47 +0000

    feat(recover_comp): implementação do botão
    - o botão vai enviar o nome do email para users/auth/password/forgot

--- frontend/src/components/layout/auth/recover-form.tsx
+++ frontend/src/components/layout/auth/recover-form.tsx
@@ -16,12 +16,70 @@ import {
   FieldLabel,
 } from "@/components/ui/forms/field"
 import { Input } from "@/components/ui/forms/input"
+import {toast} from "sonner";
+import {usersAxios} from "@/lib/axiosAPI";
+import {useState} from "react";
+
 
 export function RecoverForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
-    
+    const [isTryingRegister, setIsTryingRegister] = useState(false);
+
+    // TODO: Refactor implementation to simplify and improve error handling
+    async function handleRegister() {
+        if (isTryingRegister) return; // Prevent multiple register attempts at the same time
+
+
+        const email = (document.getElementById("email") as HTMLInputElement).value
+
+        if (!email){
+            toast.warning("Prenche todas as celulas");
+
+            return
+        }
+
+        setIsTryingRegister(true);
+
+        await usersAxios.post("/auth/password/forgot", JSON.stringify({email}), {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => {
+                if (response.status === 200) { // Assuming a successful login returns a 200 status code
+                    console.log("Recuperação efetuada!", response.data);
+                    toast.error("Pedido enviado com sucesso!");
+                }
+                else {
+                    console.log("resposta:", response);
+                }
+            })
+            .catch(error => {
+                console.error("error:", error);
+                if (error.response) {
+                    console.error("Mensagem do servidor:", error.response.data);
+                    switch (error.response.status) {
+                        case 401: toast.error("Email ou senha incorretos. Tente novamente."); break;
+                        case 404: toast.error("Usuário não encontrado. Verifique seu email ou registre-se."); break;
+                        default: toast.error("Erro no servidor. Status: " + error.response.status); break;
+                    }
+                }
+                else if (error.request) {
+                    console.error("Requisição feita, mas sem resposta:", error.request);
+                    toast.error("Nenhuma resposta do servidor. Verifique sua conexão.");
+                }
+                else {
+                    console.error("Erro ao configurar a requisição :", error.message);
+                    toast.error("Erro ao configurar a requisição da mensagem: " + error.message);
+                }
+            });
+
+        setIsTryingRegister(false);
+    }
+
+
     return (
         <div className={cn("flex flex-col gap-6", className)}>
             <BrutalCard>
