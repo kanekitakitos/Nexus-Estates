d16a49e70b9b476a35800bb9fc6c949c622f092b
truta02 <167800591+truta02@users.noreply.github.com>
2026-03-03 09:50:37 +0000

    ficheiro temporario para facilitar dar acesso á pagina de edição de propreidade

---
+++ frontend/src/app/edit/page.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { AppShell } from "@/components/layout/app-shell"
+import { BookingView } from "@/components/layout/booking/booking-view"
+import { 
+  Breadcrumb, 
+  BreadcrumbItem, 
+  BreadcrumbLink, 
+  BreadcrumbList, 
+  BreadcrumbPage, 
+  BreadcrumbSeparator 
+} from "@/components/ui/navigation/breadcrumb"
+import { PropertyEdit } from "@/components/layout/properti/property-edit"
+
+import { BookingProperty } from "@/components/layout/booking/booking-card"
+
+const prop : BookingProperty =     {
+        id: "18",
+        title: "Lakefront Cottage",
+        description: "Peaceful cottage right on the water's edge. Enjoy fishing, kayaking, or just watching the sunset.",
+        location: "Lake Tahoe, NV",
+        price: 280,
+        imageUrl: "https://images.unsplash.com/photo-1564013799919-ab600027ffc6?q=80&w=1000&auto=format&fit=crop",
+        status: "AVAILABLE",
+        rating: 4.6,
+        tags: ["Lakefront", "Sunset View", "Kayak Included"]
+    }
+
+
+export default function Home() {
+  const header = (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem className="hidden md:block">
+          <BreadcrumbLink href="#">Nexus Estates</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator className="hidden md:block" />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Bookings</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
+
+  return (
+    <AppShell header={header}>
+      <PropertyEdit property={ prop } onBack={()=>{}} />
+    </AppShell>
+  )
+}
