1424612810ceb4bb2adc00fdaa88d33770027ac7
truta02 <167800591+truta02@users.noreply.github.com>
2026-03-19 10:20:18 +0000

    fix: logica errada

--- frontend/src/components/layout/properti/property-list.tsx
+++ frontend/src/components/layout/properti/property-list.tsx
@@ -46,6 +46,7 @@ export function PropertyList(
             </>
         )
     }
+            animate && (isExiting ? "animate-fly-out-right" : "animate-fly-in"),
 
     return(
         <div id="abc" className={cn(
