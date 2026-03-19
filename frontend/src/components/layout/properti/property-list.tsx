73517246e5a3076ce23d9330e789438c81527075
truta02 <167800591+truta02@users.noreply.github.com>
2026-03-19 09:25:16 +0000

    fix: bug que podia causar aparecer 2 scroolBars

--- frontend/src/components/layout/properti/property-list.tsx
+++ frontend/src/components/layout/properti/property-list.tsx
@@ -5,14 +5,14 @@ import { ArrowLeft, MapPin, Star, Users, Home, Maximize, Check } from "lucide-re
 import { BrutalButton, Button } from "@/components/ui/forms/button"
 import { Badge } from "@/components/ui/badge"
 import { BrutalCard, BrutalShard } from "@/components/ui/data-display/card"
-import { BookingProperty } from "../booking/booking-card"
+import { BookingProperty } from "../booking/components/booking-card"
 import { cn } from "@/lib/utils"
-import { DateRangeCalendar } from "../booking/date-range-calendar"
+import { DateRangeCalendar } from "../booking/components/date-range-calendar"
 import { PropertyEditForm } from "./property-form"
 import { Separator } from "@/components/ui/layout/separator"
 import { BookingDetailsProps } from "./property-edit2"
 
-const PAGE_CONTAINER_STYLES = "flex flex-col space-y-6 p-4 md:p-6 lg:px-[150px] min-h-screen overflow-x-hidden"
+const PAGE_CONTAINER_STYLES = "grid grid-flow-row justify-center space-y-6 p-4 md:p-6 lg:px-[150px] min-h-screen"
 
 
 function NewPropertyData(id:string) {
@@ -32,7 +32,9 @@ function NewPropertyData(id:string) {
 
 
 
-export function PropertyList({propertys, onSelect = ()=>{}, isExiting}: {propertys:BookingProperty[], onSelect: (id:string) => void, isExiting : boolean}){
+export function PropertyList(
+    {propertys, onSelect = ()=>{}, isExiting=true, animate=false}: {propertys:BookingProperty[], onSelect?: (id:string) => void, isExiting? : boolean, animate?:boolean}
+){
     const [editFormOpen, setEditFormOpen] = useState<boolean>(false)
 
 
@@ -48,53 +50,50 @@ export function PropertyList({propertys, onSelect = ()=>{}, isExiting}: {propert
     return(
         <div id="abc" className={cn(
             PAGE_CONTAINER_STYLES,
-            isExiting ? "animate-fly-out-right" : "animate-fly-in",
+            isExiting && animate ? "animate-fly-out-right" : "animate-fly-in",
         )}>
             
             <AddNewPropertyForm id={`${propertys.length}`}/>
             
-            
-            <div id="gridf" className="grid grid-flow-row gap-5 p-4">
-                
+            <BrutalShard 
+                className="flex gap-0 group items-stretch p-1" 
+            >
+                <p className="w-2/5 uppercase font-bold">TITLE</p>
+                <Separator orientation="vertical" className="h-20 mx-4 bg-black" />
+                <p className="w-2/5 uppercase font-bold">location</p>
+
+                <p className="w-1/5 uppercase font-bold"></p> 
+
+            </BrutalShard>
+
+            <BrutalCard className="gap-2">
+                <BrutalButton onClick={()=>setEditFormOpen(true)} className="w-full">
+                + ADD NEW PROPERTY
+                </BrutalButton>
+            </BrutalCard>
+
+            {
+            propertys.map((prop) => (
                 <BrutalShard 
-                    className="flex gap-0 group items-stretch p-1" 
+                    key={prop.id} 
+                    className="flex gap-0 group items-stretch p-1"
+                    onClick={()=>{onSelect(prop.id)}} 
                 >
-                    <p className="w-2/5 uppercase font-bold">TITLE</p>
+                    <p className="w-2/5 uppercase">{prop.title}</p>
                     <Separator orientation="vertical" className="h-20 mx-4 bg-black" />
-                    <p className="w-2/5 uppercase font-bold">location</p>
 
-                    <p className="w-1/5 uppercase font-bold"></p> 
-
-                </BrutalShard>
-
-                <BrutalCard className="gap-2">
-                    <BrutalButton onClick={()=>setEditFormOpen(true)} className="w-full">
-                    + ADD NEW PROPERTY
-                    </BrutalButton>
-                </BrutalCard>
-
-                {
-                propertys.map((prop) => (
-                    <BrutalShard 
-                        key={prop.id} 
-                        className="flex gap-0 group items-stretch p-1"
-                        onClick={()=>{onSelect(prop.id)}} 
+                    <p className="w-2/5 uppercase">{prop.location}</p>
+                
+                    <BrutalButton
+                        className="w-1/5 bg-destructive opacity-0 group-hover:opacity-100 transition-opacity"
+                        onClick={(e)=>e.stopPropagation()}
                     >
-                        <p className="w-2/5 uppercase">{prop.title}</p>
-                        <Separator orientation="vertical" className="h-20 mx-4 bg-black" />
+                        X
+                    </BrutalButton>
+                </BrutalShard>
+                ))
+            }
 
-                        <p className="w-2/5 uppercase">{prop.location}</p>
-                    
-                        <BrutalButton
-                            className="w-1/5 bg-destructive opacity-0 group-hover:opacity-100 transition-opacity"
-                            onClick={(e)=>e.stopPropagation()}
-                        >
-                            X
-                        </BrutalButton>
-                    </BrutalShard>
-                    ))
-                }
-            </div>
         </div>
     )
 }
