2fc9341bf0847ecc42ab958c2d76628121e32b61
Luís Moreira <luisdogmoreira@gmail.com>
2026-05-04 17:14:52 +0100

    Atualização de documentação (booking-service)
    Atualizar a documentação de algumas classes no módulo booking-service

--- backend/booking-service/src/main/java/com/nexus/estates/client/Proxy.java
+++ backend/booking-service/src/main/java/com/nexus/estates/client/Proxy.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Component;
  * Atua como um ponto único de injeção para os serviços de domínio, facilitando o acesso
  * às ‘interfaces’ de comunicação definida em {@link NexusClients}.
  * </p>
- *
+ *@param propertyClient Cliente HTTP declarativo para obter cotações e regras do property-service
+ *@param userClient     Cliente HTTP declarativo para obter dados (ex: email) do user-service
+ *@param financeClient  Cliente HTTP declarativo para delegar orquestração financeira ao finance-service
  * @author Nexus Estates Team
  * @version 1.0
  */
